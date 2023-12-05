21f9b02bc0ed5f0069f6bf62a3372c503234abeb
TaeHyun Yun <yunnina01@naver.com>
2023-12-05 23:54:54 +0900

    thymeleaf 연습 및 활용(글 수정/등록)

--- springboot-blog/src/main/java/org/example/springbootblog/controller/BlogViewController.java
+++ springboot-blog/src/main/java/org/example/springbootblog/controller/BlogViewController.java
@@ -3,11 +3,13 @@ package org.example.springbootblog.controller;
 import lombok.RequiredArgsConstructor;
 import org.example.springbootblog.domain.Article;
 import org.example.springbootblog.dto.ArticleListViewResponse;
+import org.example.springbootblog.dto.ArticleViewResponse;
 import org.example.springbootblog.service.BlogService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -30,5 +32,16 @@ public class BlogViewController {
         Article article = blogService.findById(id);
         model.addAttribute("article", article);
         return "article";
+    }
+
+    @GetMapping("/new-article")
+    public String newArticle(@RequestParam(required = false) Long id, Model model) {
+        if (id == null) {
+            model.addAttribute("article", new ArticleViewResponse());
+        } else {
+            Article article = blogService.findById(id);
+            model.addAttribute("article", new ArticleViewResponse(article));
+        }
+        return "newArticle";
     }
 }
