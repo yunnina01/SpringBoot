532ca9b4d8013d5ddd74b2a7e2aaa238625b25f1
TaeHyun Yun <yunnina01@naver.com>
2023-12-08 16:35:09 +0900

    스프링 시큐리티 연습(회원가입/로그인/로그아웃)

---
+++ springboot-blog/src/main/java/org/example/springbootblog/service/UserDetailService.java
@@ -0,0 +1,19 @@
+package org.example.springbootblog.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.springbootblog.domain.User;
+import org.example.springbootblog.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserDetailService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public User loadUserByUsername(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException((email)));
+    }
+}
---
+++ springboot-blog/src/main/java/org/example/springbootblog/service/UserService.java
@@ -0,0 +1,22 @@
+package org.example.springbootblog.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.springbootblog.domain.User;
+import org.example.springbootblog.dto.AddUserRequest;
+import org.example.springbootblog.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public Long save(AddUserRequest dto) {
+        return userRepository.save(User.builder()
+                .email(dto.getEmail())
+                .password(bCryptPasswordEncoder.encode(dto.getPassword()))
+                .build()).getId();
+    }
+}
